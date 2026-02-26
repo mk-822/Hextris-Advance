1aeec9b04d3bf88e2b3a460fbfc715c71c58c0df
ＭＫ <mst.kato@gmail.com>
2026-02-27 00:05:23 +0900

    Use Butano generic_data_bin for data and score loading

--- src/DataFileLoader.cpp
+++ src/DataFileLoader.cpp
@@ -5,6 +5,67 @@
 #include "stdafx.h"
 #include "DataFileLoader.h"
 #include "common.h"
+#include "generic_data_bin.h"
+#include <cstdlib>
+
+namespace
+{
+
+class data_parser
+{
+public:
+    data_parser(const unsigned char* data, const int size) :
+        _current(reinterpret_cast<const char*>(data)),
+        _end(reinterpret_cast<const char*>(data) + size)
+    {
+    }
+
+    bool read_int(int& value)
+    {
+        _skip_white_space();
+
+        if(_current >= _end)
+        {
+            value = 0;
+            return false;
+        }
+
+        char* parse_end = nullptr;
+        value = static_cast<int>(strtol(_current, &parse_end, 10));
+
+        if(parse_end == _current)
+        {
+            value = 0;
+            return false;
+        }
+
+        _current = parse_end;
+        return true;
+    }
+
+private:
+    void _skip_white_space()
+    {
+        while(_current < _end)
+        {
+            const char chr = *_current;
+
+            if(chr == ' ' || chr == '\n' || chr == '\r' || chr == '\t')
+            {
+                ++_current;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    const char* _current;
+    const char* _end;
+};
+
+}
 
 //////////////////////////////////////////////////////////////////////
 // 構築/消滅
@@ -15,63 +76,37 @@ static const int CENTER_Y = 2;
 
 DataFileLoader::DataFileLoader()
 {
-	//ブロック・ブロック補正情報を定義ファイルから読み込み
-	static FILE *fp;
-	fp = fopen("./Data/rotation.bin","r");
-	for(int i=0 ; i<19 ; i++){
-		fscanf(fp , "%d %d %d %d",
-			&posData[i].x,
-			&posData[i].y,
-			&posData[i].next,
-			&posData[i].previous
-		);
-	}
-	fclose(fp);
+	// Butano が自動生成する generic_data_bin から読み込み
+	data_parser parser(generic_data_bin, generic_data_bin_size);
 
-	fp = fopen("./Data/blockdef.bin","r");
+	for(int i=0 ; i<19 ; i++){
+		parser.read_int(posData[i].x);
+		parser.read_int(posData[i].y);
+		parser.read_int(posData[i].next);
+		parser.read_int(posData[i].previous);
+	}
+
 	for(int i=0 ; i<10 ; i++){
-		fscanf(fp , "%d %d %d %d",
-			&blockData[i].pos[0],
-			&blockData[i].pos[1],
-			&blockData[i].pos[2],
-			&blockData[i].pos[3]
-		);
+		parser.read_int(blockData[i].pos[0]);
+		parser.read_int(blockData[i].pos[1]);
+		parser.read_int(blockData[i].pos[2]);
+		parser.read_int(blockData[i].pos[3]);
 		blockData[i].center_x = CENTER_X;
 		blockData[i].center_y = CENTER_Y;
 		blockData[i].color = i;
 	}
-	fclose(fp);
 
-	fp = fopen("./Data/grade.bin","r");
 	for(int i=0 ; i<8 ; i++){
-		fscanf(fp,"%d",&borderScore[i]);
+		parser.read_int(borderScore[i]);
 	}
-	fclose(fp);
 
 	for(int i=0 ; i<4 ; i++){
-		switch(i){
-		case 0:
-			fp = fopen("./Data/easy.bin","r");
-			break;
-		case 1:
-			fp = fopen("./Data/normal.bin","r");
-			break;
-		case 2:
-			fp = fopen("./Data/master.bin","r");
-			break;
-		case 3:
-			fp = fopen("./Data/death.bin","r");
-			break;
-		}
 		for(int j=0 ; j<10 ; j++){
-			fscanf(fp , "%d %d %d %d %d",
-				&difficultyData[i][j].fix,
-				&difficultyData[i][j].erase,
-				&difficultyData[i][j].appear,
-				&difficultyData[i][j].fall,
-				&difficultyData[i][j].g
-			);
+			parser.read_int(difficultyData[i][j].fix);
+			parser.read_int(difficultyData[i][j].erase);
+			parser.read_int(difficultyData[i][j].appear);
+			parser.read_int(difficultyData[i][j].fall);
+			parser.read_int(difficultyData[i][j].g);
 		}
-		fclose(fp);
 	}
 }
